fab2855e5ca58e7d3cae7f1b7e7d589b2a27279d
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2019-11-22 10:05:36 +0100

    Update fleet-reborn.js

--- fleet-reborn.js
+++ fleet-reborn.js
@@ -1,4 +1,4 @@
-const version = "Dragapult gang"
+const version = "Dragapult Desperados"
 try
 {
 	Config = require('./config.json')
@@ -100,7 +100,7 @@ function banlogger(a, bot, currentserver, banfile, bans, user, joinedserver)
 						var type = "green";
 						break;
 				}
-				CM(logchannel, `Flag of type ${type} has been triggered.\n<@${user.id}> ${user.id} \`<@${user.username}>#<@${user.discriminator}>\` joined ${joinedserver.name}\nBans: ${banproto.length}\nBan logger: ${banned_in}\n${avi}`)
+				CM(dvalogchannel, `Flag of type ${type} has been triggered.\n<@${user.id}> ${user.id} \`${user.username}>#<@${user.discriminator}\` \nJoined ${joinedserver.name}\nBans: ${banproto.length}\nBan logger: ${banned_in}\n${avi}`)
 			}
 			if (banproto.length > 2)
 			{
