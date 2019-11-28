595f92a495e8fbf5d30b9838596942509b863dad
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2019-11-28 14:00:27 +0100

    Update fleet-reborn.js

--- fleet-reborn.js
+++ fleet-reborn.js
@@ -3046,7 +3046,7 @@ function startscrapers()
 		snomposter(["330777938226184192","407381049908264973"],
 			   "/snomposting/status/", "https://twitter.com/snomposting","lastsnompost","New Snompost!\n",30)
 		snomposter(["446847460468457473"],
-			   "/_Pokedex_Facts/status/", "https://twitter.com/_Pokedex_Facts","lastpokedexfact","Pokedex Update!\n",1200)
+			   "/_Pokedex_Facts/status/", "https://twitter.com/_Pokedex_Facts","lastpokedexfact","Pokedex Update!\n",3600)
 		//faposter(["logchannel"],
 		//	   "/view/", "user","user","New post by User!\n",1200)
 }
