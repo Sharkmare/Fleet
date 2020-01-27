0989e6850c440e9984f639816405a310a38ef69b
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2020-01-27 10:26:39 +0100

    Update fleet-reborn.js

--- fleet-reborn.js
+++ fleet-reborn.js
@@ -1,4 +1,4 @@
-const version = `Lint-eleon`
+const version = `China`
 
 try
 {
@@ -66,10 +66,11 @@ bot.connect(
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+/*
 client.on('channelUpdate', e =>{ if(e.guild.id == '180538463655821312') {CM('323951163752054785',"A Channel was moved:\nChannel "+e.name+"\n Moved to "+e.position)
 									//console.log(e)
 									}
-			       })
+			       })*/
 bot.isFirstConnect = 1
 bot.Dispatcher.on("GATEWAY_READY", e =>
 {
