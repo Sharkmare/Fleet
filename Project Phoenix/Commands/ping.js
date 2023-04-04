4d90811d11031f4893b11ac1cd0ebe360110b268
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2023-04-04 10:34:06 +0200

    fancy ping

--- Project Phoenix/Commands/ping.js
+++ Project Phoenix/Commands/ping.js
@@ -1,11 +1,18 @@
 module.exports = {
   name: 'ping',
   aliases: ['pong'],
-  description: 'Responds with pong',
-  roleNeeded: 'admin',
+  description: 'Responds with pong and bot latency',
+  roleNeeded: null,
   permissionNeeded: 'SEND_MESSAGES',
   altFlag: 1,
   execute(message, args) {
-    message.channel.send('pong');
+    // Send a message indicating that the bot is pinging
+    message.channel.send('Pinging...').then((pingMessage) => {
+      // Calculate the bot's latency
+      const latency = pingMessage.createdTimestamp - message.createdTimestamp;
+
+      // Edit the original message to show the latency
+      pingMessage.edit(`Pong! Latency: ${latency}ms`);
+    });
   },
 };
